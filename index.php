19fdcf9beee2e560745c9d32acb7df9037d0aa7d
Pavlo Cherevach <65763670+pf4pad@users.noreply.github.com>
2024-03-21 23:47:53 +0200

    Api

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
 
-include __DIR__ . '/vendor/autoload.php';
-
-//include "src/api/random-animail.phtml";
+// include __DIR__ . '/vendor/autoload.php';
 
 
+// include __DIR__ . './api/random-animal.phtml';
+$rawApiResponse = file_get_contents('https://dog.ceo/api/breeds/image/random');
+$apiResponse = json_decode($rawApiResponse, true);
+var_export($apiResponse);
+
+echo $apiResponse["message"];
